b628fce25b127cf8db86f54af41b2ba40ca359f2
PerfectByPixel <140796026+PerfectByPixel@users.noreply.github.com>
2023-09-22 21:55:24 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const navToggle = document.querySelector('.mobile-nav-toggle');
+const primaryNav = document.querySelector('.primary-navigation');
+
+navToggle.addEventListener('click', () => {
+	primaryNav.hasAttribute("data-visible") 
+	? navToggle.setAttribute("aria-expanded", false) 
+	: navToggle.setAttribute("aria-expanded", true);
+	primaryNav.toggleAttribute("data-visible");
+});
+
+
+const items = document.querySelectorAll('.accordion button');
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+items.forEach((item) => item.addEventListener('click', toggleAccordion));
